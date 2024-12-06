6368467f49a5e43f44d05e2662a104675ed8a4e6
James Parrish <javparrish@gmail.com>
2024-12-06 23:21:29 +0000

    Day 3 - Stage 2

--- AdventOfCode/Days/Day3.cs
+++ AdventOfCode/Days/Day3.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using AdventOfCode.Enums;
 using AdventOfCode.Helpers;
+using AdventOfCode.Models.Day3;
 
 namespace AdventOfCode.Days
 {
@@ -29,13 +30,37 @@ namespace AdventOfCode.Days
 
             var lines = GetLines(Day, StageEnum.Stage2);
 
-            Console.WriteLine($"Day 2 pending");
+            var inputData = string.Join(null, lines);
+
+            var multiplicationTotal = CalculateConditionalMultiplicationTotal(inputData);
+
+            Console.WriteLine($"Multiplication total: {multiplicationTotal}");
         }
 
         private int CalculateMultiplicationTotal(string inputData)
         {
             var matchCollection = GetMultiplicationFunctions(inputData);
 
+            int multiplicationSum = GetMultiplicationSum(matchCollection);
+
+            return multiplicationSum;
+        }
+
+        private int CalculateConditionalMultiplicationTotal(string inputData)
+        {
+            var matchCollection = GetMultiplicationFunctions(inputData);
+
+            var doBands = GetDoBands(inputData);
+
+            var matchesWithinDoBands = matchCollection.Where(m => doBands.Any(b => b.StartIndex < m.Index && b.EndIndex > m.Index));
+
+            int multiplicationSum = GetMultiplicationSum(matchesWithinDoBands);
+
+            return multiplicationSum;
+        }
+
+        private int GetMultiplicationSum(IEnumerable<Match> matchCollection)
+        {
             var multiplicationSum = 0;
 
             foreach (var match in matchCollection)
@@ -64,6 +89,59 @@ namespace AdventOfCode.Days
             var secondValue = Int32.Parse(factors.Last().Value);
 
             return (firstValue, secondValue);
+        }
+
+        private IEnumerable<DoBand> GetDoBands(string input)
+        {
+            var doIndexes = GetDoIndexes(input);
+            var doNotIndexes = GetDoNotIndexes(input);
+
+            var doBands = new List<DoBand>();
+
+            if (doIndexes.First() > doNotIndexes.First())
+            {
+                doBands.Add(new DoBand()
+                {
+                    StartIndex = 0,
+                    EndIndex = doNotIndexes.First()
+                });
+            }
+
+            foreach (var doIndex in doIndexes)
+            {
+                if (doBands.Exists(b => b.StartIndex < doIndex && b.EndIndex > doIndex))
+                {
+                    continue;
+                }
+
+                var nextDoNotIndex = doNotIndexes.FirstOrDefault(i => i > doIndex);
+
+                doBands.Add(new DoBand
+                {
+                    StartIndex = doIndex,
+                    EndIndex = nextDoNotIndex == 0 ? int.MaxValue : nextDoNotIndex
+                });
+            }
+
+            return doBands;
+        }
+
+        private IEnumerable<int> GetDoIndexes(string input)
+        {
+            const string doFunctionPattern = @"do\(\)";
+            var doFunctions = Regex.Matches(input, doFunctionPattern);
+
+
+            return doFunctions.Select(m => m.Index);
+        }
+
+        private IEnumerable<int> GetDoNotIndexes(string input)
+        {
+            const string doNotFunctionPattern = @"don\'t\(\)";
+            var doNotFunctions = Regex.Matches(input, doNotFunctionPattern);
+
+
+            return doNotFunctions.Select(m => m.Index);
         }
     }
 }
---
+++ AdventOfCode/Models/Day3/DoBand.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Models.Day3
+{
+    public class DoBand
+    {
+        public int StartIndex { get; set; }
+        public int EndIndex { get; set; }
+    }
+}
